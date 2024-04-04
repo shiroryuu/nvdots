0ad9ef42f03d54c1f1be3e1df358ec1392938a87
Arvind H Nair <arvdev96@gmail.com>
2024-04-05 00:48:26 +0530

    Add Feat:Zenmode
    Signed-off-by: Arvind H Nair <arvdev96@gmail.com>

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -8,4 +8,23 @@ return {
 		end,
 		opts = {},
 	},
+	{
+		"folke/zen-mode.nvim",
+		dependencies = { "folke/twilight.nvim" },
+		config = function()
+			vim.keymap.set("n", "<Leader>tz", function()
+				require("zen-mode").setup({
+					window = {
+						backdrop = 1,
+						width = 120,
+						height = 1,
+						options = {
+							signcolumn = "no",
+							relativenumber = false,
+						},
+					},
+				})
+			end, { desc = "Toggle Zen mode" })
+		end,
+	},
 }
