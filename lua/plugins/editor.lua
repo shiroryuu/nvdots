d57a685b5812884d3ebed301b7a95e2677d8bbb6
Arvind H Nair <arvdev96@gmail.com>
2024-04-04 19:01:31 +0530

    Add Feat:Comment.nvim
    Signed-off-by: Arvind H Nair <arvdev96@gmail.com>

---
+++ lua/plugins/editor.lua
@@ -0,0 +1,16 @@
+return {
+    "numToStr/Comment.nvim",
+    dependencies = {
+        { "JoosepAlviste/nvim-ts-context-commentstring", lazy = true, opts = { enable_autocmd = false } },
+    },
+    keys = {
+        { "gc", mode = { "n", "v" }, desc = "Comment toggle linewise" },
+        { "gb", mode = { "n", "v" }, desc = "Comment toggle blockwise" },
+    },
+    opts = function(_, opts)
+        local commentstring_avail, commentstring = pcall(require, "ts_context_commentstring.integrations.comment_nvim")
+        if commentstring_avail then
+            opts.pre_hook = commentstring.create_pre_hook()
+        end
+    end,
+}
