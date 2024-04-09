73449518c82ba293962c617ccba8b2aa8bbff7ce
Arvind H Nair <arvdev96@gmail.com>
2024-04-09 17:17:10 +0530

    feat: TODO.nvim
    Signed-off-by: Arvind H Nair <arvdev96@gmail.com>

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -8,6 +8,15 @@ return {
 		end,
 		opts = {},
 	},
+    {
+        "folke/todo-comments.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        }
+    },
 	{
 		"folke/zen-mode.nvim",
 		dependencies = { "folke/twilight.nvim" },
