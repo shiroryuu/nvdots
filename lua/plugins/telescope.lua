fc1ebbaf3be9b98cd6037afd8d424461ecebead3
Arvind H Nair <arvdev96@gmail.com>
2024-04-10 20:17:11 +0530

    feat: Telescope will loaded on keypress
    telescope will not be loaded when not required.
    
    Signed-off-by: Arvind H Nair <arvdev96@gmail.com>

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,47 +1,45 @@
--- TODO: Use Telescope's mapping options and Remove the event
+-- TODO: Add scroll binding in Telescope
 return {
 	"nvim-telescope/telescope.nvim",
 	tag = "0.1.6",
 	dependencies = {
 		{ "nvim-lua/plenary.nvim", lazy = true },
 	},
-	event = "VeryLazy",
+    cmd = "Telescope",
+    keys = {
+    },
+        keys = {
+        { "<Leader>f<CR>", function() require("telescope.builtin").resume() end, mode = "n", desc = "Resume previous search" },
+        -- List open buffers
+        {"<M-Tab>", function()
+            if #vim.api.nvim_list_bufs() > 1 then
+                builtin.buffers({ sort_mru = true, ignore_current_buffer = true })
+            else
+                vim.notify("No other buffers open !!")
+            end
+        end, mode = {"i","n"}, { desc = "List all open buffers" } },
+        -- Find
+        { "<Leader>ff", function() require("telescope.builtin").find_files() end, mode = "n", desc = "Find Files" },
+        { "<Leader>fh", function() require("telescope.builtin").help_tags() end, mode = "n", desc = "Find Help" },
+        { "<Leader>fg", function() require("telescope.builtin").git_files() end, mode = "n", desc = "Find Git Files" },
+        { "<Leader>ft", function() require("telescope.builtin").colorscheme({enable_preview = true}) end, mode = "n", desc = "Find Themes" },
+        -- TODO: Change this to fs and FS and use live_grep in fw
+        { "<Leader>fw", function()
+			local word = vim.fn.expand("<cword>")
+            require("telescope.builtin").grep_string({ search = word})
+        end, mode = "n", desc = "Find Word" },
+        { "<Leader>fW", function()
+			local word = vim.fn.expand("<cWORD>")
+            require("telescope.builtin").grep_string({ search = word})
+        end, mode = "n", desc = "Find Word" },
+        -- Git
+        { "<Leader>gb", function() require("telescope.builtin").git_branches() end, mode = "n", desc = "Git Branches" },
+        { "<Leader>gc", function() require("telescope.builtin").git_commits() end, mode = "n", desc = "Git Commits (Repo)" },
+        { "<Leader>gC", function() require("telescope.builtin").git_bcommits() end, mode = "n", desc = "Git Commits (Current File)" },
+        { "<Leader>gt", function() require("telescope.builtin").git_status() end, mode = "n", desc = "Git Status" },
+    },
+    -- TODO: Add opts in future
 	config = function()
 		require("telescope").setup({})
-		local builtin = require("telescope.builtin")
-		local map = vim.keymap.set
-		map("n", "<Leader>f<CR>", builtin.resume, { desc = "Resume previous search" })
-		map("n", "<Leader>ff", builtin.find_files, { desc = "Find files" })
-		map("n", "<Leader>fh", builtin.help_tags, { desc = "Find help" })
-		map("n", "<Leader>fg", builtin.git_files, { desc = "Find git files" })
-		map("n", "<Leader>gb", builtin.git_branches, { desc = "Git Branches" })
-		map("n", "<Leader>gc", builtin.git_commits, { desc = "Git Commits (Repo)" })
-		map("n", "<Leader>gC", builtin.git_bcommits, { desc = "Git Branches (Current File)" })
-		map("n", "<Leader>gt", builtin.git_status, { desc = "Git Status" })
-		map("n", "<Leader>ft", function()
-			builtin.colorscheme({ enable_preview = true })
-		end, { desc = "Find Themes" })
-		map({ "i", "n" }, "<M-Tab>", function()
-			if #vim.api.nvim_list_bufs() > 1 then
-				builtin.buffers({ sort_mru = true, ignore_current_buffer = true })
-			else
-				vim.notify("No other buffers open !!")
-			end
-		end, { desc = "List all open buffers" })
-		map("n", "<Leader>fw", function()
-			local word = vim.fn.expand("<cword>")
-			builtin.grep_string({ search = word })
-		end, { desc = "Find word" })
-		map("n", "<Leader>fws", function()
-			local word = vim.fn.expand("<cWORD>")
-			builtin.grep_string({ search = word })
-		end, { desc = "Find String" })
-		map("n", "<leader>fW", function()
-			builtin.live_grep({
-				additional_args = function(args)
-					return vim.list_extend(args, { "--hidden", "--no-ignore" })
-				end,
-			})
-		end, { desc = "Find words in all files" })
 	end,
 }
