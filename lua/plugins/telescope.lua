1983705f7de3bd331e48c891564f58821ab87ed5
Arvind H Nair <arvdev96@gmail.com>
2024-04-12 18:16:16 +0530

    Update:Telescope
    Signed-off-by: Arvind H Nair <arvdev96@gmail.com>

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,42 +5,43 @@ return {
 		{ "nvim-lua/plenary.nvim", lazy = true },
 	},
     cmd = "Telescope",
-    keys = {
-    },
-        keys = {
-        { "<Leader>f<CR>", function() require("telescope.builtin").resume() end, mode = "n", desc = "Resume previous search" },
-        -- List open buffers
-        {"<M-Tab>", function()
-            if #vim.api.nvim_list_bufs() > 1 then
-                builtin.buffers({ sort_mru = true, ignore_current_buffer = true })
-            else
-                vim.notify("No other buffers open !!")
-            end
-        end, mode = {"i","n"}, { desc = "List all open buffers" } },
-        -- Find
-        { "<Leader>ff", function() require("telescope.builtin").find_files() end, mode = "n", desc = "Find Files" },
-        { "<Leader>fh", function() require("telescope.builtin").help_tags() end, mode = "n", desc = "Find Help" },
-        { "<Leader>fg", function() require("telescope.builtin").git_files() end, mode = "n", desc = "Find Git Files" },
-        { "<Leader>ft", function() require("telescope.builtin").colorscheme({enable_preview = true}) end, mode = "n", desc = "Find Themes" },
-        { "<Leader>fw", function() require("telescope.builtin").live_grep() end, mode = "n", desc = "Find Word" },
-        { "<Leader>fW", function()
-            require("telescope.builtin").live_grep({
-            additional_args = function(args) return vim.list_extend(args, { "--hidden", "--no-ignore" }) end, })
+    keys = function()
+        local builtin = require("telescope.builtin")
+        return {
+            { "<Leader>f<CR>", function() builtin.resume() end, mode = "n", desc = "Resume previous search" },
+            -- List open buffers
+            {"<M-Tab>", function()
+                if #vim.api.nvim_list_bufs() > 1 then
+                    builtin.buffers({ sort_mru = true, ignore_current_buffer = true })
+                else
+                    vim.notify("No other buffers open !!")
+                end
+            end, mode = {"i","n"}, { desc = "List all open buffers" } },
+            -- Find
+            { "<Leader>ff", function() builtin.find_files() end, mode = "n", desc = "Find Files" },
+            { "<Leader>fh", function() builtin.help_tags() end, mode = "n", desc = "Find Help" },
+            { "<Leader>fg", function() builtin.git_files() end, mode = "n", desc = "Find Git Files" },
+            { "<Leader>ft", function() builtin.colorscheme({enable_preview = true}) end, mode = "n", desc = "Find Themes" },
+            { "<Leader>fw", function() builtin.live_grep() end, mode = "n", desc = "Find Word" },
+            { "<Leader>fW", function()
+                builtin.live_grep({
+                    additional_args = function(args) return vim.list_extend(args, { "--hidden", "--no-ignore" }) end, })
             end, mode = "n", desc = "Find Word in all files" },
-        { "<Leader>fs", function()
-			local word = vim.fn.expand("<cword>")
-            require("telescope.builtin").grep_string({ search = word})
-        end, mode = "n", desc = "Search Word" },
-        { "<Leader>fS", function()
-			local word = vim.fn.expand("<cWORD>")
-            require("telescope.builtin").grep_string({ search = word})
-        end, mode = "n", desc = "Search String" },
-        -- Git
-        { "<Leader>gb", function() require("telescope.builtin").git_branches() end, mode = "n", desc = "Git Branches" },
-        { "<Leader>gc", function() require("telescope.builtin").git_commits() end, mode = "n", desc = "Git Commits (Repo)" },
-        { "<Leader>gC", function() require("telescope.builtin").git_bcommits() end, mode = "n", desc = "Git Commits (Current File)" },
-        { "<Leader>gt", function() require("telescope.builtin").git_status() end, mode = "n", desc = "Git Status" },
-    },
+            { "<Leader>fs", function()
+                local word = vim.fn.expand("<cword>")
+                builtin.grep_string({ search = word})
+            end, mode = "n", desc = "Search Word" },
+            { "<Leader>fS", function()
+                local word = vim.fn.expand("<cWORD>")
+                builtin.grep_string({ search = word})
+            end, mode = "n", desc = "Search String" },
+            -- Git
+            { "<Leader>gb", function() builtin.git_branches() end, mode = "n", desc = "Git Branches" },
+            { "<Leader>gc", function() builtin.git_commits() end, mode = "n", desc = "Git Commits (Repo)" },
+            { "<Leader>gC", function() builtin.git_bcommits() end, mode = "n", desc = "Git Commits (Current File)" },
+            { "<Leader>gt", function() builtin.git_status() end, mode = "n", desc = "Git Status" },
+        }
+    end,
     opts = function()
         local actions = require("telescope.actions")
         return {
