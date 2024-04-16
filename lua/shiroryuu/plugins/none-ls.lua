56d3c2f09182307a923a1c2c53b22e586489dfba
Arvind H Nair <arvdev96@gmail.com>
2024-04-16 20:03:57 +0530

    Add feat: None-ls
    Signed-off-by: Arvind H Nair <arvdev96@gmail.com>

---
+++ lua/shiroryuu/plugins/none-ls.lua
@@ -0,0 +1,25 @@
+return {
+    "nvimtools/none-ls.nvim",
+    dependencies = {
+        { "nvim-lua/plenary.nvim", lazy = true },
+        {
+            "jay-babu/mason-null-ls.nvim",
+            dependencies = { "williamboman/mason.nvim" },
+            cmd = { "NullLsInstall", "NullLsUninstall" },
+            init = function(plugin) require("shiroryuu.utils.plugin").on_load("mason.nvim", plugin.name) end,
+            opts = { handlers = {} },
+        }
+    },
+    main = "null-ls",
+    event = "User LazyFile",
+    opts = function(_, opts)
+        local null_ls = require("null-ls")
+        opts.sources = vim.list_extend(opts.sources or {}, {
+            null_ls.builtins.formatting.yamlfix,
+            null_ls.builtins.formatting.stylua,
+            null_ls.builtins.formatting.shfmt,
+            null_ls.builtins.diagnostics.zsh,
+        })
+
+    end,
+}
