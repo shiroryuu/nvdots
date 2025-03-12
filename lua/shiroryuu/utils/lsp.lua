73c4003ea9b8ce7405609ba01f6aa7470cfbfca7
Arvind H Nair <arvdev96@gmail.com>
2025-03-13 00:39:57 +0530

    (feat) Add inlay hints support
    Neovim 0.10 supports inlay hints which is enabled by default now
    
    Signed-off-by: Arvind H Nair <arvdev96@gmail.com>

--- lua/shiroryuu/utils/lsp.lua
+++ lua/shiroryuu/utils/lsp.lua
@@ -16,4 +16,20 @@ end
 
 -- TODO: Separate api for on_supports_method
 
+---@param method string
+---@param fn fun(client:vim.lsp.Client, buffer)
+function M.on_supports_method(method, fn)
+  M._supports_method[method] = M._supports_method[method] or setmetatable({}, { __mode = "k" })
+  return vim.api.nvim_create_autocmd("User", {
+    pattern = "LspSupportsMethod",
+    callback = function(args)
+      local client = vim.lsp.get_client_by_id(args.data.client_id)
+      local buffer = args.data.buffer ---@type number
+      if client and method == args.data.method then
+        return fn(client, buffer)
+      end
+    end,
+  })
+end
+
 return M
