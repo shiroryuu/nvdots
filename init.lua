add72db43817ed738598144892878fcb9a6f4511
Arvind H Nair <arvdev96@gmail.com>
2025-05-15 14:31:37 +0530

    Update TODOs
    Signed-off-by: Arvind H Nair <arvdev96@gmail.com>

--- init.lua
+++ init.lua
@@ -1,15 +1,15 @@
 -- TODO: Update nvim to V0.11
 -- TODO: Add keymap for lsp signature help
 -- TODO: Add toggle term
--- TODO: Try blink.cmp
--- TODO: Port which-key to new specs and Fix the overlap warnings
--- DONE: Move to oil.nvim
--- TODO: Advance Telescope multigrep
--- REFR: TJ Devries patch, https://github.com/tjdevries/config.nvim/blob/master/lua/custom/plugins/telescope.lua
 -- NOTE: https://github.com/AstroNvim/AstroNvim/blob/cc6dc97f0f059a4ba5dda916ffc137d6609fa0ff/lua/astronvim/plugins/toggleterm.lua#L20
+-- DONE: Try blink.cmp
+-- TODO: Port which-key to new specs (DONE) and Fix the overlap warnings (PENDING)
+-- DONE: Move to oil.nvim
+-- DONE: Advance Telescope multigrep
+-- REFR: TJ Devries patch, https://github.com/tjdevries/config.nvim/blob/master/lua/custom/plugins/telescope.lua
 -- DONE: Add python support
 -- DONE: Add Snacks.nvim
--- TODO: Replace none-ls.nvim with nvim-lint & conform.nvim
+-- DONE: Replace none-ls.nvim with nvim-lint & conform.nvim
 -- TODO: Rethink vim mappings
 for _, source in ipairs({
 	"shiroryuu.options",
