7cf6a929ec1ad591ff08fb54b966da23a0858cd5
Arvind H Nair <arvdev96@gmail.com>
2026-08-27 14:53:23 +0530

    feat(colorscheme): Add colorschemes

---
+++ core/colorscheme.nix
@@ -0,0 +1,35 @@
+{ lib, pkgs, ... }:
+{
+  config.vim = {
+    startPlugins = [ pkgs.vimPlugins.kanagawa-nvim ];
+    luaConfigRC.theme = lib.nvim.dag.entryBefore [ "pluginConfigs" "lazyConfigs" ] ''
+      -- Kanagawa theme
+      require('kanagawa').setup({
+        compile = false,             -- enable compiling the colorscheme
+        theme = "dragon",
+        undercurl = true,            -- enable undercurls
+        commentStyle = { italic = true },
+        functionStyle = {},
+        keywordStyle = { italic = true},
+        statementStyle = { bold = true },
+        typeStyle = {},
+        transparent = false,         -- do not set background color
+        dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
+        terminalColors = true,       -- define vim.g.terminal_color_{0,17}
+        colors = {                   -- add/modify theme and palette colors
+            palette = {},
+            theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+        },
+        overrides = function(colors) -- add/modify highlights
+            return {}
+        end,
+        background = {               -- map the value of 'background' option to a theme
+            dark = "dragon",           -- try "dragon" !
+            light = "lotus"
+        },
+      })
+      -- setup must be called before loading
+      vim.cmd.colorscheme("kanagawa")
+    '';
+  };
+}
