714e036c1dd91c1e2de3514cad094367125e9c34
Arvind H Nair <arvdev96@gmail.com>
2026-09-03 15:19:37 +0530

    chore: update TODOs

--- langs/nix.nix
+++ langs/nix.nix
@@ -8,7 +8,7 @@
         };
         lsp = {
             enable = true;
-            servers = ["nil"];
+            servers = ["nixd"];
         };
     };
 }
--- plugins/editor/todo.nix
+++ plugins/editor/todo.nix
@@ -89,9 +89,10 @@
         multiline_pattern = "^."; # lua pattern to match the next multiline from the start of the matched keyword
         multiline_context = 10; # extra lines that will be re-evaluated when changing a line
         before = ""; # "fg" or "bg" or empty
-        keyword = "wide"; # "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
+        keyword = "wide_bg"; # "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
         after = "fg"; # "fg" or "bg" or empty
-        pattern = ".*<(KEYWORDS)\s*:"; # pattern or table of patterns, used for highlighting (vim regex)
+        pattern = ".*<(KEYWORDS)[^:]*:";
+        # pattern = ".*<(KEYWORDS)\\s*([^)]*):"; # pattern or table of patterns, used for highlighting (vim regex)
         comments_only = true; # uses treesitter to match keywords in comments only
         max_line_len = 400; # ignore lines longer than this
         exclude = { }; # list of file types to exclude highlighting
@@ -105,7 +106,8 @@
           "--column"
         ];
         command = "${pkgs.ripgrep.out}/bin/rg";
-        pattern = "\\b(KEYWORDS)(\\([^\\)]*\\))?:";
+        pattern = "\\b(KEYWORDS)[^:]*:";
+        # pattern = "\\b(KEYWORDS)(\\([^\\)]*\\))?";
       };
 
     };
